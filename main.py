5a76f862530efb47531dd4223250d65f0211c7e0
Rafael <rafaelcgama@gmail.com>
2025-03-28 13:59:34 -0300

    added but commented email functionality

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import os
 import random
+# import smtplib
+# from email.mime.multipart import MIMEMultipart
+# from email.mime.base import MIMEBase
+# from email import encoders
 import pandas as pd
 from time import time
 from unidecode import unidecode
@@ -10,6 +14,10 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions as EC
+
+# EMAIL_SEND_FROM = ""
+# EMAIL_SEND_FROM_PASSWORD = ""
+# EMAIL_SEND_TO = ""
 
 
 # ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
@@ -171,6 +179,60 @@ def save_hotel_price_date(results: Dict[str, Union[int, float]]) -> None:
     print("")
 
 
+# def create_email_msg():
+#     server = smtplib.SMTP(host='smtp.gmail.com', port=587)
+#     server.starttls()
+#
+#     server.login(EMAIL_SEND_FROM, EMAIL_SEND_FROM_PASSWORD)
+#
+#     msg = MIMEMultipart()  # create a message object
+#
+#     # Setup the parameters of the message
+#     msg.update({
+#         'From': EMAIL_SEND_FROM,
+#         'To': EMAIL_SEND_TO,
+#         'Subject': f'Data Report {datetime.now().strftime("%Y/%d/%m")}'
+#     })
+#
+#     # Attach the body with the msg instance
+#     body = "Please find the attached zip file with the latest data."
+#     msg.attach(body)
+#
+#     # Specify the path to your zip file
+#     filename = "/Users/rafaelcgama/Projects/hotels/data/data.zip"
+#
+#     # Open the zip file in binary mode
+#     with open(filename, "rb") as attachment:
+#         part = MIMEBase("application", "octet-stream")
+#         part.set_payload(attachment.read())
+#
+#     # Encode the attachment in base64
+#     encoders.encode_base64(part)
+#
+#     # Add header with file name
+#     part.add_header(
+#         "Content-Disposition",
+#         f"attachment; filename={filename.split('/')[-1]}",  # Extracts the file name from the path
+#     )
+#
+#     # Attach the part (zip file) to the message
+#     msg.attach(part)
+#
+#     return server, msg
+#
+#
+# def send_email(server, msg):
+#     # Send the email
+#     try:
+#         server.sendmail(msg['From'], msg['To'], msg.as_string())
+#         print("Email sent successfully!")
+#     except Exception as e:
+#         print(f"Error sending email: {e}")
+#
+#     # Quit the SMTP session
+#     server.quit()
+
+
 # ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
 # Main Execution
 # ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
@@ -209,6 +271,9 @@ if __name__ == "__main__":
     driver.quit()
 
     save_hotel_price_date(results)
+
+    # server, msg = create_email_msg()
+    # send_email(server, msg)
 
     print("")
     print(f"\n⏱ Time taken: {round(time() - t1, 2)}s")
--- run_hotels.sh
+++ run_hotels.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-cd /Users/rafaelcgama/Projects/hotels
-source .venv/bin/activate
+
+# Set up paths
+PROJECT_DIR="" # Change to the path where the hotels project is stored
+ARCHIVE_FILE="$PROJECT_DIR/data/data.zip"  # This will store the compressed file
+
+# Activate your virtual environment
+source "$PROJECT_DIR/.venv/bin/activate"
 python main.py
+
+# Combine the current output with previous results and create a compressed archive
+cd "$PROJECT_DIR/data"
+
+# Append the new output to the previous one and create the zip archive
+zip -r "$ARCHIVE_FILE" *.csv  # Compress all CSVs into a zip archive
+
+# Deactivate the virtual environment (optional)
+deactivate
